b113324930971484ff39c6b6df59ec5bd054ab81
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2022-02-18 22:22:13 -0800

    added latest time property

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -68,8 +68,12 @@ router.get("/", async (req, res, next) => {
       }
 
       // set properties for notification count and latest message preview
-      convoJSON.latestMessageText = convoJSON.messages[0].text;
+      let lastIndex = convoJSON.messages.length-1;
+      convoJSON.latestMessageText = convoJSON.messages[lastIndex].text;
       conversations[i] = convoJSON;
+
+      //set a property for latest message time
+      convoJSON.latestMessageTime = convoJSON.messages[lastIndex].createdAt;
     }
 
     res.json(conversations);
