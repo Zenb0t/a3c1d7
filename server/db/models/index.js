7d39d5fd843440fd045c714eefea3abb8a1d10df
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2022-02-25 22:23:42 -0800

    db associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,17 +1,29 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
+const Group = require("./group");
 
 // associations
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
-Message.belongsTo(Conversation);
+Group.belongsToMany(User, { through: "groupMembers" });
+User.belongsToMany(Group, { through: "groupMembers" });
+
+User.hasMany(Group, { as: "owner" });
+Group.belongsTo(User, { as: "owner" });
+
+Group.hasMany(Conversation);
+Conversation.belongsTo(Group);
+
 Conversation.hasMany(Message);
+Message.belongsTo(Conversation);
+
+Message.belongsTo(User, { as: "sender" });
+User.hasMany(Message, { as: "sender" });
+
 
 module.exports = {
   User,
   Conversation,
-  Message
+  Message,
+  Group
 };
